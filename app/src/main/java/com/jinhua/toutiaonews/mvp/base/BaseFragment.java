9ecad52e192e1a190e5ecb4433e53f1276101c80
杨金花 <jinhua.yang@BKJK.COM>
2018-06-27 17:51:59 +0800

    add

--- app/src/main/java/com/jinhua/toutiaonews/mvp/base/BaseFragment.java
+++ app/src/main/java/com/jinhua/toutiaonews/mvp/base/BaseFragment.java
@@ -1,6 +1,15 @@
 package com.jinhua.toutiaonews.mvp.base;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 
 public abstract class BaseFragment extends Fragment {
+
+    private BasePresenterActivity mActivity;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (BasePresenterActivity)context;
+    }
 }
--- app/src/main/java/com/jinhua/toutiaonews/mvp/base/BasePresenterFragment.java
+++ app/src/main/java/com/jinhua/toutiaonews/mvp/base/BasePresenterFragment.java
@@ -1,7 +1,28 @@
 package com.jinhua.toutiaonews.mvp.base;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 public abstract class BasePresenterFragment<V extends BaseContract.BaseView,T extends BasePresenter<V>> extends BaseFragment{
 
+    private T mPresenter;
+    public abstract T createPresenter();
 
+    public T getPresenter(){
+        return mPresenter;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPresenter = createPresenter();
+        mPresenter.attachView((V)this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPresenter.detachView();
+    }
 }
